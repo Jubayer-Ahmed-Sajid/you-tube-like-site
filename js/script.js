9e4bc2ec5579b1d62d8ed598939ef6a1324f55ea
Jubayer-Ahmed-Sajid <jubayerahmed661ac@gmail.com>
2023-09-01 15:30:22 +0600

    almost finished

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ const handleCategory = async () => {
         const tab = categories.category
         const div = document.createElement('div')
         div.innerHTML = `
-        <button class ="btn active:bg-red-400 ml-6 px-8"  onclick = handleCategoryId(${categories.category_id});>${tab}
+        <button class ="btn active:bg-red-400 ml-2 px-2 lg:ml-6 lg:px-8"  onclick = handleCategoryId(${categories.category_id});>${tab}
         </button>
         `
         categoryContainer.appendChild(div);
@@ -34,15 +34,15 @@ const handleCategoryId = async (categoryId) => {
         const div = document.createElement('div');
         div.innerHTML = `
             <div class="card card-compact rounded-lg ">
-                <figure>
-                        <img src =${cards.thumbnail} class = "w-[312px] h-[200px]">
+                <div>
+                        <img src =${cards.thumbnail} class = "h-[200px]">
                         <div>
                             ${cards.others.posted_date ? `
-                             <div class="bg-black text-white rounded-lg absolute bottom-3 right-3 p-2">
+                             <div class="bg-black text-white rounded-lg absolute lg:bottom-3 lg:right-3 p-2">
                              <p id="posted">${Math.floor(cards.others.posted_date / 3600)}hrs ${Math.floor((cards.others.posted_date % 3600) / 60)}min</p>
                         </div>
                     ` : ''}
-                </figure>
+                </div>
                 </div>
 
             <div class="flex gap-3 mt-4 items-center">
@@ -62,14 +62,14 @@ const handleCategoryId = async (categoryId) => {
 
     });
     console.log(cards)
-    
+
     if (cards.length === 0) {
         const empty = document.getElementById('card-container');
         const errorMessage = document.createElement('div');
         errorMessage.innerHTML = `
           <div class="text-center">
-            <img src="images/icon.png" alt="Error Icon" class="mb-4 ml-[500px]"/>
-            <h2 class="text-4xl font-bold ml-[400px] w-96"> Oops!! Sorry, There is no content here</h2>
+            <img src="images/icon.png" alt="Error Icon" class="mb-4 ml-32 lg:ml-[500px]"/>
+            <h2 class="text-4xl font-bold lg:ml-[400px] w-96"> Oops!! Sorry, There is no content here</h2>
           </div>
         `;
 
@@ -81,24 +81,95 @@ const handleCategoryId = async (categoryId) => {
 
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Function to sort the cards by view
+function sortByViews(cards) {
+    return cards.sort((a, b) => {
+        const viewsA = parseInt(a.others.views.replace(/[^\d]/g, ''));
+        const viewsB = parseInt(b.others.views.replace(/[^\d]/g, ''));
+        return viewsB - viewsA;
+    });
+}
+
+// Defining a variable to store the card's data
+let cardsData = [];
+
+// Function to display cards
+function displayCards(cards) {
+    const cardContainer = document.getElementById('card-container');
+    cardContainer.innerText = '';
+
+    cards.forEach((cards) => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+      <div class="card card-compact rounded-lg ">
+      <figure>
+              <img src =${cards.thumbnail} class = "w-[312px] h-[200px]">
+              <div>
+                  ${cards.others.posted_date ? `
+                   <div class="bg-black text-white rounded-lg absolute bottom-3 right-3 p-2">
+                   <p id="posted">${Math.floor(cards.others.posted_date / 3600)}hrs ${Math.floor((cards.others.posted_date % 3600) / 60)}min</p>
+              </div>
+          ` : ''}
+      </figure>
+      </div>
+
+  <div class="flex gap-3 mt-4 items-center">
+      <img src = ${cards.authors[0].profile_picture} class = "w-10 h-10 rounded-[40px]"/>
+      <h4 class ="font-bold">${cards.title} </h4>
+  </div>
+  <div class = "flex gap-2 items-center"> 
+      <h5 class = "text-sm text text-gray-500 ml-14 mt-2 mb-2">${cards.authors[0].profile_name}</h5>
+      <div>
+          ${cards.authors[0].verified ? '<i class="fas fa-check-circle" style="color: #0255e3;"></i>' : ''}
+      </div>
+  </div>
+   <h5 class = "text-sm text text-gray-500 ml-14 mb-2">${cards.others.views} <span>  views </span></h5>
+</div>
+`
+            ;
+        cardContainer.appendChild(div);
+    });
+}
+
+// Attach click event listener to the "Sort by Views" button
+const sortByViewsButton = document.getElementById('sortByViewsButton');
+sortByViewsButton.addEventListener('click', () => {
+    // Sort the cards by views
+    const sortedCards = sortByViews(cardsData);
+
+    // Display the sorted cards
+    displayCards(sortedCards);
+});
+
+// Fetch and populate the card data (replace this with your actual data fetching code)
+const handleSortView = async () => {
+    const response = await fetch('https://openapi.programming-hero.com/api/videos/category/1000');
+    const data = await response.json();
+    cardsData = data.data; // Assign the fetched data to the cardsData variable
+    displayCards(cardsData); // Display the initial cards
+};
+
+// Call the fetchData function to fetch and display the initial card data
+handleSortView('https://openapi.programming-hero.com/api/videos/category/1000');
+
+
 handleCategory();
-handleCategoryId(1000);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// handleCategoryId(1000);
 
 
 
