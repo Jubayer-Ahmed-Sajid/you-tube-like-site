9d3c46739b53719dfec886d0a67dc90b6c6d6577
Jubayer-Ahmed-Sajid <jubayerahmed661ac@gmail.com>
2023-09-02 18:12:09 +0600

    code modified

--- js/script.js
+++ js/script.js
@@ -68,7 +68,7 @@ const handleCategoryId = async (categoryId) => {
         const errorMessage = document.createElement('div');
         errorMessage.innerHTML = `
           <div class="text-center">
-            <img src="./images/icon.png" alt="Error Icon" class="mb-4 ml-32 lg:ml-[500px]"/>
+            <img src="js/icon.png" alt="Error Icon" class="mb-4 ml-32 lg:ml-[500px]"/>
             <h2 class="text-4xl font-bold lg:ml-[400px] w-96"> Oops!! Sorry, There is no content here</h2>
           </div>
         `;
